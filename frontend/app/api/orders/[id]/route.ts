e4c5f878aec8457194a2856ebe837d7051462b41
michael-ambel <michaelambel46@gmail.com>
2024-12-21 15:39:14 +0300

    orderApiSlice, orderSlice, orderTypes, Order API Route Handlers for Place, Get, Update and Delete done

---
+++ frontend/app/api/orders/[id]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+//..get order by id
+export async function GET(req: Request) {
+  const urlParts = req.url.split("/");
+  const id = urlParts[urlParts.length - 1];
+
+  try {
+    const resp = await axios.get(`http://localhost:5000/api/orders/${id}`);
+    return NextResponse.json(resp.data, { status: resp.status });
+  } catch (error) {
+    const message =
+      axios.isAxiosError(error) && error.response
+        ? error.response.data
+        : (error as Error).message;
+    return NextResponse.json({ message }, { status: 500 });
+  }
+}
+
+//delete order
+export async function DELETE(req: Request) {
+  const urlParts = req.url.split("/");
+  const id = urlParts[urlParts.length - 1];
+
+  try {
+    const resp = await axios.get(`http://localhost:5000/api/orders/${id}`);
+    return NextResponse.json(resp.data, { status: resp.status });
+  } catch (error) {
+    const message =
+      axios.isAxiosError(error) && error.response
+        ? error.response.data
+        : (error as Error).message;
+    return NextResponse.json({ message }, { status: 500 });
+  }
+}
---
+++ frontend/app/api/orders/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+//..place orders
+export async function POST(req: Request) {
+  try {
+    const body = req.json();
+    const resp = await axios.post("http://localhost:5000/api/orders", body);
+    return NextResponse.json(resp.data, { status: resp.status });
+  } catch (error) {
+    const message =
+      axios.isAxiosError(error) && error.response
+        ? error.response.data
+        : (error as Error).message;
+    return NextResponse.json({ message }, { status: 500 });
+  }
+}
+
+//..get all orders
+export async function GET() {
+  try {
+    const resp = await axios.get("http://localhost:5000/api/orders");
+    return NextResponse.json(resp.data, { status: resp.status });
+  } catch (error) {
+    const message =
+      axios.isAxiosError(error) && error.response
+        ? error.response.data
+        : (error as Error).message;
+    return NextResponse.json({ message }, { status: 500 });
+  }
+}
